8e89eeb52f8abf012def08bbc75b22fcab4da046
htranhp1213 <linhhy99@gmail.com>
2023-07-30 23:32:33 -0400

    changes

--- database/starting_data.sql
+++ database/starting_data.sql
@@ -11,16 +11,16 @@ INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_n
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `image_url`, `category`)
-VALUES ('Fruit Sponge Cake', 'A soft and light airy whipped cream with fruit cake ', 20.00, 'static/images/fruitCake.jpeg', 'Cake');
+VALUES ('Fruit Sponge Cake', 'A soft and light airy whipped cream with fruit cake ', 30.00, 'static/images/fruitCake.jpeg', 'Cake');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `image_url`, `category`)
-VALUES ('Cupcake', 'Pink cupcake with marble cream cheese frosting (sale by dozen)', 10.00, 'static/images/cupcakes.jpeg', 'Cupcake');
+VALUES ('Cupcake', 'Pink cupcake with marble cream cheese frosting (sale by dozen)', 20.00, 'static/images/cupcake.png', 'Cupcake');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `image_url`, `category`)
-VALUES ('Milk Bread', 'Signature milk bread', 4.00, 'static/images/milkBread.jpeg', 'Bread');
+VALUES ('Milk Bread', 'Signature milk bread', 5.00, 'static/images/milkBread.jpeg', 'Bread');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `image_url`, `category`)
-VALUES ('French Bread', 'Baguette', 1.00, 'static/images/frenchBread.jpeg', 'Bread');
+VALUES ('French Bread', 'Baguette', 4.00, 'static/images/frenchBread.jpeg', 'Bread');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
