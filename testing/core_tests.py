92aba309703edf12b2088eab2ef076fcffa73367
Zarin Khan <zarinkhan8425@gmail.com>
2023-08-06 22:35:58 -0400

    changes

--- testing/core_tests.py
+++ testing/core_tests.py
@@ -127,7 +127,7 @@ def test_get_session_db() -> tuple:
     
     '''
     -------------------------------------
-    -----------REVIEW-TESTINGS------------
+    -----------REVIEW-TESTINGS-----------Zarin Khan---------------
     -------------------------------------
     '''
 def test_review_submission() -> tuple:
